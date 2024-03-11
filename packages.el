7d8e0a3cd0ee0f1a6f0701023611c31bc896f7f8
Nicholai <nicholai@comfy.sh>
2024-03-11 13:06:20 -0400

    caddy syntax highlighting

--- packages.el
+++ packages.el
@@ -33,6 +33,9 @@
 (package! lsp-tailwindcss
   :recipe (:host github :repo "merrickluo/lsp-tailwindcss"))
 
+(package! caddyfile-mode)
+
+
 ;; If the package you are trying to install does not contain a PACKAGENAME.el
 ;; file, or is located in a subdirectory of the repo, you'll need to specify
 ;; `:files' in the `:recipe':
